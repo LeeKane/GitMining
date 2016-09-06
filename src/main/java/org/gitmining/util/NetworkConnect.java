d5d893342630165dfb0a337fc7b3b7079d16f4cb
lk14 <lk14@software.nju.edu.cn>
2016-09-06 18:03:16 +0800

    chart data json

--- src/main/java/org/gitmining/util/NetworkConnect.java
+++ src/main/java/org/gitmining/util/NetworkConnect.java
@@ -13,7 +13,7 @@ import com.google.gson.Gson;
 public class NetworkConnect {
 	public static Gson gson = new Gson();
 
-	public static String token = "0ac2b35cdc54dbbabcebe6ca135ed743c6a03d28";
+	public static String token = "23dc756c0cec82526d1070adc181e75cb0d6aacc";
 
 
 	public static String getJson(String httpUrl, String httpArg) {
